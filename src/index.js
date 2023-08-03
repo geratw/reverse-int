d7b3239acd32e83bfd9360ac286129b75ea4edc0
Maksim <maks.orehovskii@mail.ru>
2023-08-04 00:50:24 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
+    const reversedString = Math.abs(n).toString().split('').reverse().join('');
+    return +reversedString;
+  };
   
-}
